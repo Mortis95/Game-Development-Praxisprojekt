d2b95b61c376e7ce174383735a3894532150c89e
RealM_ <moritz.stadje@stud.uni-due.de>
2022-01-09 13:06:34 +0100

    Wasserhieb hinzugefügt
    - Benötigt weitere Arbeit

---
+++ Assets/Ressources/Scripts/Skills/WasserHieb.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WasserHieb : MonoBehaviour
+{
+
+    public Sprite WasserHiebUpwards;
+    public Sprite WasserHiebLeft;
+    public Sprite WasserHiebRight;
+    public Sprite WasserHiebDownwards;
+    
+    private void Awake(){
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
