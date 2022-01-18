a7f6e68708c4d2f7f8b5d21cade999864a8769b5
BMGforever <alex.simon.1001@gmail.com>
2022-01-18 11:54:00 +0100

    textures dungeon 1

--- Assets/Resources/Scripts/PuzzleDungeon1/PuzzleController.cs
+++ Assets/Resources/Scripts/PuzzleDungeon1/PuzzleController.cs
@@ -9,7 +9,15 @@ public class PuzzleController : MonoBehaviour
     public bool p3 = false;
     public bool p4 = false;
 
+    public GameObject trigger;
+    public GameObject blockade;
+
     public int id=0;    
+    
+    void Start()
+    {
+        trigger.SetActive(false);
+    }
     
     void Update()
     {
@@ -19,6 +27,8 @@ public class PuzzleController : MonoBehaviour
             {
                 Debug.Log("Puzzlegeschafft");
                 //SzenenÜbergang instantiieren
+                trigger.SetActive(true);
+                blockade.SetActive(false);
                 //Animation
                 //Sound
                 Destroy(this);
@@ -31,6 +41,8 @@ public class PuzzleController : MonoBehaviour
             {
                 Debug.Log("Puzzlegeschafft");
                 //SzenenÜbergang instantiieren
+                trigger.SetActive(true);
+                blockade.SetActive(false);
                 //Animation
                 //Sound
                 //truhe öffnen
@@ -42,7 +54,8 @@ public class PuzzleController : MonoBehaviour
             if(p1 & p2 & p3 & p4)
             {
                 Debug.Log("Puzzlegeschafft");
-                //SzenenÜbergang instantiieren
+                trigger.SetActive(true);
+                blockade.SetActive(false);
                 //Animation
                 //Sound
                 //Truhe öffnen
