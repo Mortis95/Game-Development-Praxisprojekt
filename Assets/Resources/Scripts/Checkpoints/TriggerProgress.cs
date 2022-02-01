31b2c97b7725d78d8612ec1df32222605653e3a6
BMGforever <alex.simon.1001@gmail.com>
2022-02-01 16:05:26 +0100

    triggerprogress eingerichtet

---
+++ Assets/Resources/Scripts/Checkpoints/TriggerProgress.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerProgress : MonoBehaviour
+{
+    public KeyCode interactionKey;    
+    public int id;
+    public bool isInRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isInRange)
+        {
+            if(Input.GetKeyDown(interactionKey) )
+            {
+                if(id == 1)
+                {
+                Progress.getInstance().check1=true;
+                }
+                if(id == 2)
+                {
+                    Progress.getInstance().check2=true;
+                }
+                if(id == 3)
+                {
+                    Progress.getInstance().check2=true;
+                }else{
+                    Debug.Log("Da ist deine id in den fritten");
+                }        
+
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            isInRange= true;
+            //Debug.Log("in range");
+
+        }
+    }
+
+     private void OnTriggerExit2D(Collider2D collision)
+     {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            isInRange= false;           
+        }
+    }
+}
